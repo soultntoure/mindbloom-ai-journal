96dc58a2a4e6eb46f4910e1d7fa07e5cd0e89b2e
Toure <142293316+soultntoure@users.noreply.github.com>
2025-06-26 12:57:48 +0800

    feat: scaffold packages/server/src/auth/auth.controller.ts

---
+++ packages/server/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+}
